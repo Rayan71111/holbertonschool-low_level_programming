217368f9bd69565bb07f4e0f2fd72b89d8b97611
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-20 12:46:59 +0000

    ajoute la tache 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	printf("      /\\\n");
+	printf("     /  \\\n");
+	printf("    /----\\\n");
+	printf("   /      \\\n");
+	printf("  /--------\\\n");
+	printf(" /          \\\n");
+	printf("/____________\\\n");
+	return (0);
+}
