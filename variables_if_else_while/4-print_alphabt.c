65a7ad38c415398c7fd9aced3e6208af99be1348
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-21 09:58:16 +0000

    ajoute tache 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - affiche l'alphabet en minuscules sauf q et e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
