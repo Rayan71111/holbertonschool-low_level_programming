7c2bfac7754e507c1d6d875f146e5bba645de6be
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-22 07:56:20 +0000

    ajoute tache 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - affiche l'alphabet en minuscules 10 fois
+ * suivies chacune d'un saut de ligne
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
