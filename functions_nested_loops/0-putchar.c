2c9d95b8c541d4e296cec217de9623ab7434b67e
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-22 07:38:49 +0000

    ajoute tache 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - Point d’entrée du programme
+ *
+ * Return: Toujours 0
+ */
 int main(void)
 {
-    puts("_putchar");
-    return 0;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
