904022603cff8e50528933cbcc4630d2d5a1c9d8
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-27 14:22:39 +0000

    ajoute tache 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - dessine une ligne diagonale 
+ * @n: nombre de fois que le caractère '_' doit être affiché
+ *
+ * Return: rien (void)
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n'); /* juste un retour à la ligne si n <= 0 */
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n'); /* saut de ligne à la fin */
+}
