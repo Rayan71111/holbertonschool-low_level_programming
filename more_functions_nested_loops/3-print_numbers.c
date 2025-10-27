7c37b5ca0c98c8de09da725c9fd7f0aa5edad641
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-27 13:02:02 +0000

    ajoute tache 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - affiche les chiffres de 0 à 9
+ *
+ * Return: rien (void)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
