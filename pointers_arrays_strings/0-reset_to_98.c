f331d8d29b65f54335dcee0d1caa4a39f2450551
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-29 10:54:51 +0000

    ajoute tache 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - modifie la valeur de l'entier pointé pour qu'elle devienne 98
+ * @n: pointeur vers l'entier à modifier
+ *
+ * Return: rien
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
