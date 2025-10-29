6d45374939c5d5ee796f99b18452d82ef856ff39
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-29 14:08:33 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,21 @@
-#include "main.h"
 #include <unistd.h>
+#include "main.h"
 
+/**
+ * _puts - affiche une chaîne de caractères suivie d'un retour à la ligne
+ * @str: chaîne à afficher
+ */
 void _puts(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (!str)
-        return;
+	if (!str)
+		return;
 
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    write(1, "\n", 1);
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
