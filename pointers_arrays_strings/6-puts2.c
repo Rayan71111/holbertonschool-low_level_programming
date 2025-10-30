cb2cd57fd55efde7a2b78b314ce8795124f1c2d8
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-30 13:38:05 +0000

    ajoute tache 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - affiche un caractère sur deux d'une chaîne
+ * @str: la chaîne de caractères à afficher
+ *
+ * Retour: rien
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i += 2; /* passer un caractère sur deux */
+	}
+	putchar('\n');
+}
