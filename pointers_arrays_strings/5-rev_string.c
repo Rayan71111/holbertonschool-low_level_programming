e369bd0a6bd7b635afed3f2c80f3854fd84fb56e
Rayan71111 <Kbelhadj479@gmail.com>
2025-10-30 09:14:48 +0000

    ajoute tache 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,18 @@
  */
 void rev_string(char *s)
 {
-    int i = 0, j;
-    char temp;
+	int i = 0, j;
+	char temp;
 
-    /* Find the length of the string */
-    while (s[i] != '\0')
-        i++;
+	/* Find the length of the string */
+	while (s[i] != '\0')
+		i++;
 
-    /* Swap characters from both ends */
-    for (j = 0; j < i / 2; j++)
-    {
-        temp = s[j];
-        s[j] = s[i - j - 1];
-        s[i - j - 1] = temp;
-    }
+	/* Swap characters from both ends */
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
 }
